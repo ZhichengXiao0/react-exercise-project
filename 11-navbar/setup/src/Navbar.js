e9310e472e09faf149cc0c0a6b494fdc6c24b214
Zhicheng <shi199387@gmail.com>
2023-06-23 18:48:51 -0400

    dynamic nav links useRef,useState,useEffect

--- 11-navbar/setup/src/Navbar.js
+++ 11-navbar/setup/src/Navbar.js
@@ -1,10 +1,63 @@
-import React, { useState, useRef, useEffect } from 'react'
-import { FaBars, FaTwitter } from 'react-icons/fa'
-import { links, social } from './data'
-import logo from './logo.svg'
+import React, { useState, useRef, useEffect } from 'react';
+import { FaBars, FaTwitter } from 'react-icons/fa';
+import { links, social } from './data';
+import logo from './logo.svg';
 
 const Navbar = () => {
-  return <h4>navbar</h4>
-}
+  const [showLinks, setShowLinks] = useState(false);
+  const linkContainerRef = useRef(null);
+  const linkRef = useRef(null);
+  useEffect(() => {
+    const linksHeight = linkRef.current.getBoundingClientRect().height;
+    if (showLinks) {
+      linkContainerRef.current.style.height = `${linksHeight}px`;
+    } else {
+      linkContainerRef.current.style.height = '0px';
+    }
+  }, [showLinks]);
 
-export default Navbar
+  return (
+    <nav>
+      <div className='nav-center'>
+        <div className='nav-header'>
+          <img className='logo' src={logo} alt='coding addict' />
+          <button
+            className='nav-toggle'
+            onClick={() => setShowLinks(!showLinks)}
+          >
+            <FaBars />
+          </button>
+        </div>
+        <div
+          ref={linkContainerRef}
+          className={`${
+            showLinks ? 'links-container show-container' : 'links-container'
+          }`}
+        >
+          <ul className='links' ref={linkRef}>
+            {links.map((link) => {
+              const { id, url, text } = link;
+              return (
+                <li key={id}>
+                  <a href={url}>{text}</a>
+                </li>
+              );
+            })}
+          </ul>
+          <ul className='social-icons'>
+            {social.map((item) => {
+              const { id, url, icon } = item;
+              return (
+                <li key={id}>
+                  <a href={url}>{icon}</a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
