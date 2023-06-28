1ab2f9fb5ecb4afee25a1c804e639c8354c9e3bd
Zhicheng <shi199387@gmail.com>
2023-06-27 22:19:49 -0400

    added display submenu onMouseOver

--- 13-stripe-submenus/setup/src/Navbar.js
+++ 13-stripe-submenus/setup/src/Navbar.js
@@ -4,7 +4,11 @@ import { FaBars } from 'react-icons/fa';
 import { useGlobalContext } from './context';
 
 const Navbar = () => {
-  const { openSidebar } = useGlobalContext();
+  const { openSidebar, openSubmenu, closeSubmenu } = useGlobalContext();
+  const displaySubmenu = (e) => {
+    console.log(e.target);
+    openSubmenu();
+  };
   return (
     <nav className='nav'>
       <div className='nav-center'>
@@ -16,13 +20,19 @@ const Navbar = () => {
         </div>
         <ul className='nav-links'>
           <li>
-            <button className='link-btn'>products</button>
+            <button className='link-btn' onMouseOver={displaySubmenu}>
+              products
+            </button>
           </li>
           <li>
-            <button className='link-btn'>developers</button>
+            <button className='link-btn' onMouseOver={displaySubmenu}>
+              developers
+            </button>
           </li>
           <li>
-            <button className='link-btn'>company</button>
+            <button className='link-btn' onMouseOver={displaySubmenu}>
+              company
+            </button>
           </li>
         </ul>
         <button className='btn signin-btn'>sign in</button>
